b64b62926a1466fe51649399d5e1d5fb2f40c7f3
JunTaeHahm <ahuuae@icloud.com>
2023-03-11 16:06:31 +0900

    Feature : 노선정보 및 실시간 위치 가져오기 추가

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,62 @@
 const dayjs = require('dayjs');
 const Router = require('express');
 const axios = require('axios');
-
-require('dotenv').config();
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
 const router = Router();
 const now = dayjs();
 
-axios
-  .get(
-    `http://apis.data.go.kr/6410000/busrouteservice/getBusRouteList?serviceKey=${process.env.BUS_DATA_API_KEY}&keyword=6004`,
-  )
-  .then((res) => {
-    console.log(res.data);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+require('dotenv').config();
+
+/**
+ * @path {GET} /api/info/:number
+ * @description 노선정보 가져오기
+ */
+router.get(`/info/:number`, async (req, res, next) => {
+  try {
+    const businfoXML = await axios
+      .get(
+        `http://apis.data.go.kr/6410000/busrouteservice/getBusRouteList?serviceKey=${process.env.BUS_DATA_API_KEY}&keyword=${req.params.number}`,
+      )
+      .then((res) => {
+        console.log(res.data);
+        return res.data;
+      });
+
+    /* XML to JSON */
+    parser.parseStringPromise(businfoXML).then(function (result) {
+      const businfoJSON = JSON.stringify(result);
+
+      return res.status(200).send(businfoJSON);
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * @path {GET} /api/location/:id
+ * @description 노선 실시간 위치 가져오기
+ */
+router.get(`/location/:id`, async (req, res, next) => {
+  try {
+    const businfoXML = await axios
+      .get(
+        `http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList?serviceKey=${process.env.BUS_DATA_API_KEY}&routeId=${req.params.id}`,
+      )
+      .then((res) => {
+        return res.data;
+      });
+
+    /* XML to JSON */
+    parser.parseStringPromise(businfoXML).then(function (result) {
+      const businfoJSON = JSON.stringify(result);
+
+      return res.status(200).send(businfoJSON);
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
